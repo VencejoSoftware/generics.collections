ecf8caeeb1e390d956d2dfa6a3ee3df696aa997f
Vencejo Software <vencejosoftware@gmail.com>
2018-05-19 10:50:46 -0300

    Added batch to update forked repository

---
+++ update_forked.bat
@@ -0,0 +1,5 @@
+git remote add --track master maciej-izak https://github.com/maciej-izak/generics.collections.git
+git remote
+git fetch maciej-izak
+git pull maciej-izak master
+git merge maciej-izak/master
